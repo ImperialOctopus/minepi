f9c7fb6cc9b6c1be3aad3b2f2da6d29e74695d1b
Zoey Child <imperialoctopus@gmail.com>
2021-03-06 20:15:00 +0000

    Move routes

--- flutter/lib/routes/menu/menu_route.dart
+++ flutter/lib/routes/menu/menu_route.dart
@@ -1,11 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MenuRoute extends StatelessWidget {
+/// Route for menu page.
+class MenuRoute extends StatefulWidget {
+  /// Route for menu page.
   const MenuRoute();
 
   @override
+  _MenuRouteState createState() => _MenuRouteState();
+}
+
+class _MenuRouteState extends State<MenuRoute> {
+  late final TextEditingController _textController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Column(
+      children: [
+        const Text('Menu'),
+        TextField(
+          controller: _textController,
+        ),
+        ElevatedButton(
+          onPressed: () => BlocProvider.of<KeyBloc>(context)
+              .add(KeySubmitted(loginKey: _textController.text)),
+          child: const Text('Submit'),
+        )
+      ],
+    );
   }
 }
