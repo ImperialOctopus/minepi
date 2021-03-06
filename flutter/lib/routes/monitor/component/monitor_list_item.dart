8f036e4b1ac86b0465db654ae30db8dd77a744a9
Zoey Child <imperialoctopus@gmail.com>
2021-03-06 22:16:37 +0000

    Add minecraft dye textures

--- flutter/lib/routes/monitor/component/monitor_list_item.dart
+++ flutter/lib/routes/monitor/component/monitor_list_item.dart
@@ -14,7 +14,7 @@ class MonitorListItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: Image.asset('/assets/block_icon/' + color + '.png'),
+      leading: Image.asset('/assets/dyes/' + color + '_dye.png'),
       title: Text(color),
       trailing: Text(value == 0 ? '' : value.toString()),
     );
