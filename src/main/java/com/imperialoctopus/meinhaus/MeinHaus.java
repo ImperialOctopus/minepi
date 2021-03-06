2c2e46773d54c7d139fc4784c4d22e6d1dfee528
Zoey Child <imperialoctopus@gmail.com>
2021-03-06 15:04:46 +0000

    Add example mod code

--- src/main/java/com/imperialoctopus/meinhaus/MeinHaus.java
+++ src/main/java/com/imperialoctopus/meinhaus/MeinHaus.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.imperialoctopus.meinhaus;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,13 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("meinhaus")
+public class MeinHaus
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public MeinHaus() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
