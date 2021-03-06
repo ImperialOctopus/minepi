c7487ed857a14f62dccda1a449be2a3de5de7688
Zoey Child <imperialoctopus@gmail.com>
2021-03-06 17:18:17 +0000

    Add template app layout

---
+++ flutter/lib/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+/// Default app theme
+final themeData = ThemeData(
+  // Define the default brightness and colors.
+  brightness: Brightness.light,
+  primarySwatch: Colors.indigo,
+  accentColor: Colors.indigoAccent,
+);
