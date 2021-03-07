d195c021ed51132227a8cd7a3602e03217362a1e
OliverWales <oliverfwales@gmail.com>
2021-03-07 07:27:41 +0000

    Implement fetching player data from firestore

--- python/client.py
+++ python/client.py
@@ -1,6 +1,13 @@
+import requests
+import sys
 import RPi.GPIO as GPIO
 import time
 
+if len(sys.argv) != 2:
+    print("Usage: client <username>")
+    exit()
+
+# Flash LEDs for one second to check circuit
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -12,6 +19,40 @@ GPIO.output(22,GPIO.HIGH)
 GPIO.output(23,GPIO.HIGH)
 GPIO.output(24,GPIO.HIGH)
 time.sleep(1)
+
+GPIO.output(22,GPIO.LOW)
+GPIO.output(23,GPIO.LOW)
+GPIO.output(24,GPIO.LOW)
+
+# Get player data
+username = sys.argv[1]
+x = requests.get("https://firestore.googleapis.com/v1/projects/mine-pi/databases/(default)/documents/users/" + username)
+
+if x.status_code != 200:
+  print("Error fetching player data")
+  exit()
+
+fields = x.json()['fields']
+
+print(fields)
+
+if "green" in fields and fields["green"] > 0:
+	GPIO.output(24,GPIO.HIGH)
+else
+	GPIO.output(24,GPIO.LOW)
+	
+if "yellow" in fields and fields["yellow"] > 0:
+	GPIO.output(24,GPIO.HIGH)
+else
+	GPIO.output(24,GPIO.LOW)
+
+if "red" in fields and fields["red"] > 0:
+	GPIO.output(24,GPIO.HIGH)
+else
+	GPIO.output(24,GPIO.LOW)
+
+
+time.sleep(10)
 
 GPIO.output(22,GPIO.LOW)
 GPIO.output(23,GPIO.LOW)
